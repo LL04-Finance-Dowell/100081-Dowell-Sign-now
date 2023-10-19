73e9f0f1ead97e61340d697103d8406164c1ed85
Mayorisaac24 <mayorisaac24@gmail.com>
2023-10-19 04:38:16 +0100

    mayorisaac

---
+++ app/helpers.py
@@ -0,0 +1,25 @@
+import requests
+
+def upload_pdf_and_get_url(pdf_file):
+    try:
+        url = "https://dowellfileuploader.uxlivinglab.online/uploadfiles/upload-pdf-file/"
+        
+        files = {'pdf': ('file.pdf', pdf_file)}
+        
+        response = requests.post(url, files=files)
+
+        if response.status_code == 200 or 201:
+            try:
+                response_data = response.json()
+                if 'file_url' in response_data:
+                    pdf_url = response_data['file_url']
+                    return pdf_url
+                else:
+                    return f"Error: Response does not contain 'file_url' - {response.text}"
+            except ValueError:
+                return f"Error: Response is not in JSON format - {response.text}"
+        else:
+            return f"Error: {response.status_code} - {response.text}"
+    except Exception as e:
+        return f"Error: {str(e)}"
+
--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ import os
 import re
 import uuid  # Import the uuid library
 import git
+from django.http import JsonResponse
 
 import requests
 from django.core.cache import cache
@@ -15,32 +16,55 @@ from rest_framework.response import Response
 from app.mongo_db_connection import (
     save_to_signnow_document_collection,
 )
+from app.helpers import(
+    upload_pdf_and_get_url
+)
 
 @api_view(["GET"])
 def home(request):
-    "Is server down? I though not!"
+    "Dowell signnow home directory"
     return Response(
-        "If you are seeing this :), the server is !down.", status.HTTP_200_OK
+        "Welcome.", status.HTTP_200_OK
     )
 
+    
 @api_view(["POST"])
 def create_signnow_document(request):
-    """Document Creation."""
+    """get signnow details."""
     if not request.data:
         return Response(
-            {"message": "Failed to process signnow document creation."},
+            {"message": "Failed to get signnow document information."},
             status.HTTP_200_OK,
         )
-    res = json.loads(
-        save_to_signnow_document_collection(
-            {
-               
-            }
+    if 'pdf' not in request.FILES:
+        return Response({'error': 'No PDF file was submitted'}, status=status.HTTP_400_BAD_REQUEST)
+    pdf_file = request.FILES['pdf']
+    pdf_url = upload_pdf_and_get_url(pdf_file)
+    if pdf_url:
+        res = json.loads(
+            save_to_signnow_document_collection(
+                {
+                    "file_name": "Untitled Document",
+                    "pdf_url": pdf_url,
+                }
+            )
         )
-    )
-    if res["isSuccess"]:
-        
-        return Response(
-            {"editor_link": "editor_link", "_id": res["inserted_id"]},
-            status.HTTP_201_CREATED,
-        )
+        if res["isSuccess"]:
+            return Response({"pdf_url": pdf_url}, status.HTTP_201_CREATED,)
+    else:
+        return Response({'error': 'Failed to upload the PDF'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+         
+
+    
+# @api_view(["POST"])
+# def create_signnow_document(request):
+#     if 'pdf' not in request.FILES:
+#         return Response({'error': 'No PDF file was submitted'}, status=status.HTTP_400_BAD_REQUEST)
+
+#     pdf_file = request.FILES['pdf']
+#     pdf_url = upload_pdf_and_get_url(pdf_file)
+
+#     if pdf_url:
+#         return Response({'file_url': pdf_url}, status=status.HTTP_201_CREATED)
+#     else:
+#         return Response({'error': 'Failed to upload the PDF'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
