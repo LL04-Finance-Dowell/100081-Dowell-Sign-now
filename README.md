6c0abc45f796ab77a2aa68de1e615cdb2ab7fd0a
Mayorisaac24 <mayorisaac24@gmail.com>
2023-10-25 10:17:45 +0100

    mayorisaac

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # 100081-Dowell-Sign-now
+
+Function Description
+
+    The upload_pdf_and_get_url function uploads a PDF file to a specific URL.
+    It handles various error cases and returns relevant error messages.
+
+Error Handling
+
+    If the upload is successful, it returns the URL of the uploaded PDF.
+    If the server response is not in JSON format or does not contain the 'file_url' field, it provides an appropriate error message.
+    If the server returns an error status code, it reports the error along with the response text.
+    If there is a JSONDecodeError during the response processing, it returns an internal server error response.
+
+Note
+
+Please replace 'your_file.pdf' with the actual path to the PDF file you want to upload.
--- app/views.py
+++ app/views.py
@@ -30,33 +30,39 @@ def home(request):
     
 @api_view(["POST"])
 def create_signnow_document(request):
-    """get signnow details."""
-    if not request.data:
-        return Response(
-            {"message": "Failed to get signnow document information."},
-            status.HTTP_200_OK,
-        )
-    if 'pdf' not in request.FILES:
-        return Response({'error': 'No PDF file was submitted'}, status=status.HTTP_400_BAD_REQUEST)
-    pdf_file = request.FILES['pdf']
-    pdf_url = upload_pdf_and_get_url(pdf_file)
-    if pdf_url:
-        res = json.loads(
-            save_to_signnow_document_collection(
-                {
-                    "file_name": "Untitled Document",
-                    "pdf_url": pdf_url,
-                    # "created_by": request.data["created_by"],
-                    # "company_id": organization_id,
-                    # "data_type": request.data["data_type"],
-                    # "auth_viewers": viewers,
-                }
-            )
-        )
-        print(res)
-        if res["isSuccess"]:
+    try:
+        """get signnow details."""
+        if not request.data:
             return Response(
-                {"_id": res["inserted_id"], "pdf_url": pdf_url}, status.HTTP_201_CREATED,)
-    else:
-        return Response({'error': 'Failed to upload the PDF'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-         
+                {"message": "Failed to get signnow document information."},
+                status=status.HTTP_200_OK,
+            )
+        if 'pdf' not in request.FILES:
+            return Response({'error': 'No PDF file was submitted'}, status=status.HTTP_400_BAD_REQUEST)
+        pdf_file = request.FILES['pdf']
+        pdf_url = upload_pdf_and_get_url(pdf_file)
+        if pdf_url:
+            res = json.loads(
+                save_to_signnow_document_collection(
+                    {
+                        "file_name": "Untitled Document",
+                        "pdf_url": pdf_url,
+                        # "created_by": request.data["created_by"],
+                        # "company_id": organization_id,
+                        # "data_type": request.data["data_type"],
+                        # "auth_viewers": viewers,
+                    }
+                )
+            )
+            print(res)
+            if res["isSuccess"]:
+                return Response(
+                    {"_id": res["inserted_id"], "pdf_url": pdf_url}, status=status.HTTP_201_CREATED,)
+            else:
+                return Response({'error': 'Failed to save to SignNow collection'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response({'error': 'Failed to upload the PDF'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    except json.JSONDecodeError:
+        return Response(
+            "Invalid response data", status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
